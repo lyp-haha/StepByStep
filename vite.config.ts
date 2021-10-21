1981bcf2bb5a30545bfd1e6860c1dd28b7f8a94f
lyp-haha <72698851+lyp-haha@users.noreply.github.com>
2021-10-21 11:06:31 +0800

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // publicDir:'./',
+  base:'./',
   build:{
     outDir:'./docs',
-    assetsDir:'./'
-  }
+    assetsDir:'./assets'
+  },
 })
